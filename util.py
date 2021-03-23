112a1636e2a42973cb0cb21accc0dff33a48ad58
doanthuan <doanvuthuan@gmail.com>
2021-03-23 15:44:29 +0700

    aggregate trans hist

--- util.py
+++ util.py
@@ -2,24 +2,29 @@ import pandas as pd
 from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score, precision_score, recall_score, f1_score
 import pickle
 
-def preprocess_data(train_df, account_df, sale_df, features, cat_features):
-    train_df = build_RW000076(train_df, account_df, sale_df)
-    train_df['request_date_dt'] = pd.to_datetime(train_df['request_date'], format='%Y-%m-%d %H:%M:%S')
-    #train_df['request_date'] = (train_df['request_date_dt'] - train_df['request_date_dt'].min()).days
-    train_df['date_diff'] = (train_df['request_date_dt'] - train_df['request_date_dt'].min()).dt.days
+def preprocess_data(train, account, sale, trans_hist, features, cat_features):
     
-    train_df["same_name"] = train_df["cust_name"]==train_df["ben_cust_name"]
-    train_df["same_phone"] = train_df["msisdn"]==train_df["ben_msisdn"]
-    train_df["same_phone_channel"] = train_df["msisdn"]==train_df["msisdn_channel"]
-    train_df["same_phone_channel_ben"] = train_df["ben_msisdn"]==train_df["msisdn_channel"]
+    # aggregate trans hist data
+    train = train.merge(trans_hist, how='left', left_on=['ben_msisdn'], right_on=['msisdn_'])
     
-    train_df[cat_features] = train_df[cat_features].fillna(value="")
-    #train_df[cat_features].fillna('', inplace=True)
+    #aggregate account, sale data
+    train = build_RW000076(train, account, sale)
     
     
+    train['request_date_dt'] = pd.to_datetime(train['request_date'], format='%Y-%m-%d %H:%M:%S')
+    #train['request_date'] = (train['request_date_dt'] - train['request_date_dt'].min()).days
+    train['date_diff'] = (train['request_date_dt'] - train['request_date_dt'].min()).dt.days
     
-    y = train_df['is_fraud']
-    X = train_df.drop(['is_fraud'], axis = 1)
+#     train["same_name"] = train["cust_name"]==train["ben_cust_name"]
+#     train["same_phone"] = train["msisdn"]==train["ben_msisdn"]
+#     train["same_phone_channel"] = train["msisdn"]==train["msisdn_channel"]
+#     train["same_phone_channel_ben"] = train["ben_msisdn"]==train["msisdn_channel"]
+    
+    train[cat_features] = train[cat_features].fillna(value="")
+    #train[cat_features].fillna('', inplace=True)
+    
+    y = train['is_fraud']
+    X = train.drop(['is_fraud'], axis = 1)
     X = X[features]
     return X, y
 
@@ -34,7 +39,7 @@ def split_data(data):
     return X, y, X_train, y_train, X_val, y_val, X_test, y_test
 
 
-def split_val_data(train_df):
+def split_val_data(train):
 
     X_train, X_val, y_train, y_val = train_test_split( X_train, y_train, test_size=0.2, random_state=42)
     return X_train, X_val, y_train, y_val
@@ -57,9 +62,9 @@ def save_model(model, filename):
 def load_model(filename):
     return pickle.load(open(filename, 'rb'))
 
-def build_RW000076(train_df, account_df, sale_df):
-    account_sale_df = sale_df.merge(account_df, how='inner', left_on=['account_sale_id', 'phone'], right_on=['account_sale_id', 'msisdn'])
-    account_sale_df = account_sale_df[["phone","staff_code"]]
-    train_df = train_df.merge(account_sale_df, how='left', left_on=['ben_msisdn','staff_code'], right_on=['phone','staff_code'])
-    train_df["RW000076"] = pd.notnull(train_df["phone"])
-    return train_df
+def build_RW000076(train, account, sale):
+    account_sale = sale.merge(account, how='inner', left_on=['account_sale_id', 'phone'], right_on=['account_sale_id', 'msisdn'])
+    account_sale = account_sale[["phone","staff_code"]]
+    train = train.merge(account_sale, how='left', left_on=['ben_msisdn','staff_code'], right_on=['phone','staff_code'])
+    train["RW000076"] = pd.notnull(train["phone"])
+    return train
